a5c9d6c53f1b5301fb6794e6dab258c1a41fab84
AlessandroBregoli <alessandroxciv@gmail.com>
2019-12-31 12:20:50 +0100

    Modified and executed Max_kl_distribution

--- code/max_kl_distribution.R
+++ code/max_kl_distribution.R
@@ -1,7 +1,8 @@
+library(igraph)
 library(jsonlite)
 library(dplyr)
+library(parallel)
 source("code/utils_algo.R")
-
 ck_dependency_max_kl <- function(trjs,variables, to,from,sep_set){
   cimsout_trjs = list()
   cimsout_trjs_without_to = list()
@@ -46,13 +47,16 @@ ck_dependency_max_kl <- function(trjs,variables, to,from,sep_set){
   }
   return(Ckl)  
 }
-Ckl_distribution <- function(trjs,variables){
+Ckl_distribution <- function(trjs,variables,dyn.str){
   from_ret = c()
   to_ret = c()
+  sep_set_ret = c()
   n_ret = c()
   ck_ret = c()
+  is_edge_ret = c()
+  real_edge_ret = c()
   vars = variables$Name
-  
+  net.struct = graph_from_data_frame(dyn.str)
   n = 0
   # Variables wich are already completly explored (from the entering edges point of view)
   exausted_vars = c()
@@ -75,48 +79,70 @@ Ckl_distribution <- function(trjs,variables){
             sep_set = c()
           else
             sep_set = unlist(sep_set_comb[index_set,])
+
 	  from_ret = c(from_ret, from)
 	  to_ret = c(to_ret,to)
+	  sep_set_ret = c(sep_set_ret,toString(sep_set))
 	  n_ret = c(n_ret, n)
 	  ck_ret = c(ck_ret,ck_dependency_max_kl(trjs, variables, to,from,sep_set))
+	  real_edge_ret = c(real_edge_ret,are.connected(net.struct,from,to))
+	  asp = all_simple_paths(net.struct,from,to)
+	  is_edge = FALSE
+	  for(sp in asp){
+	    if(length(intersect(as_ids(sp),sep_set)) == 0){
+	    	is_edge = TRUE
+	    	break()
+	    }
+	    
+	  }
+	  is_edge_ret = c(is_edge_ret, is_edge)
         }
       }
     }
     n = n + 1
   }
 
-  return(data.frame(From=from_ret, To=to_ret, n=n_ret, ck=ck_ret))
+  return(data.frame(From=from_ret, To=to_ret, n=n_ret, sep_set=sep_set_ret,ck=ck_ret,is_edge=is_edge_ret,real_edge=real_edge_ret,stringsAsFactors = FALSE))
   
 }
 
 
-distribution_experiment <- function(networks){
+distribution_experiment <- function(dataset_path){
   ret = list()
-  
+
+  dataset_name = substr(dataset_path, 6, nchar(dataset_path)-6)
+  print(dataset_name)
+  load(dataset_path)
   for(i in seq_along(networks)){
-    print(paste(i,"/",length(networks)))
+    print(paste(dataset_name, " - ",i,"/",length(networks)))
     samples =  networks[[i]][["samples"]]
     variables = networks[[i]][["variables"]]
 
-    ret[[i]] = Ckl_distribution(samples,variables)
+    dyn.str = networks[[i]][["dyn.str"]]
+    ret[[i]] = list()
+    for(subsample in c(10,20,50,100)){
+	tmp_samples = samples[1:subsample]
+    	ret[[i]][[paste(subsample,"_trajectories",sep="")]] = Ckl_distribution(tmp_samples,variables,dyn.str)
+    }
   }
-  return(list(ret))
+  tmp_ret = list()
+  tmp_ret[[dataset_name]] = ret
+  return(tmp_ret)
 }
 
-distribution_experiments <- function(datasets_path){
-	results = list()
-	for(dataset_path in datasets_path){
-		dataset_name = substr(dataset_path, 6, nchar(dataset_path)-6)
-		print(dataset_name)
-		load(dataset_path)
-		results[[dataset_name]] = distribution_experiment(networks)
-	}
-	save(results,file="data/Ckl_distribution.RData")
+distribution_experiments <- function(datasets_path){	
+  #distribution_experiment(datasets_path[[1]])
+  results = mclapply(datasets_path, distribution_experiment, mc.cores=8)
+  results = unlist(results, recursive=FALSE)
+  save(results,file="data/Ckl_distribution.RData")
+  rj = toJSON(results)
+  write(rj, "data/Ckl_distribution.json")
 }
 
 datasets_path =c()
 for(i in list(3,4,5,6)){
-	datasets_path = c(datasets_path, paste("data/networks_and_trajectories_var",i,".RData",sep=""))
+	datasets_path = c(datasets_path, paste("data/networks_and_trajectories_binary_data_",i,".RData",sep=""))
+	datasets_path = c(datasets_path, paste("data/networks_and_trajectories_ternary_data_",i,".RData",sep=""))
 }
 
 distribution_experiments(datasets_path)
