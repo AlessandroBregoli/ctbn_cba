d6303c782e88155da66a34f28bd0aea4ecf47d81
AlessandroBregoli <alessandroxciv@gmail.com>
2020-03-16 20:50:18 +0100

    Added python algo results

--- code/rdata_to_json.R
+++ code/rdata_to_json.R
@@ -1,6 +1,6 @@
 library("jsonlite")
 
-for(i in list(3,4,5,6,10,15)){
+for(i in list(3,4,5,6,10,15,20)){
 	dataset_path =  paste("data/networks_and_trajectories_binary_data_",i,sep="")
     load(paste(dataset_path,".RData", sep=""))
     write(toJSON(networks), paste(dataset_path, ".json", sep=""))
